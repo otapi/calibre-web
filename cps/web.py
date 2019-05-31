aeadd79afd8922e066b911aa166eca90b1993dd0
otapi <otapi@protonmail.ch>
2019-05-31 23:13:52 +0200

    Guest user now uses the proper locale settings

--- cps/web.py
+++ cps/web.py
@@ -221,8 +221,7 @@ def get_locale():
     # if a user is logged in, use the locale from the user settings
     user = getattr(g, 'user', None)
     if user is not None and hasattr(user, "locale"):
-        if user.nickname != 'Guest':   # if the account is the guest account bypass the config lang settings
-            return user.locale
+        return user.locale
     translations = [str(item) for item in babel.list_translations()] + ['en']
     preferred = list()
     for x in request.accept_languages.values():
